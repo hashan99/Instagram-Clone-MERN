da8057c31b4b66188c23f81486895908364ef780
Hashan Kumarasinghe <54454767+hashan99@users.noreply.github.com>
2020-12-04 01:33:06 +0530

    sign in route implemented

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model("User")
 const bcrypt = require('bcryptjs')
+const e = require('express')
 
 router.get('/',(req,res)=>{
     res.send("Hello")
@@ -40,6 +41,31 @@ router.post('/signup',(req,res)=>{
             console.log(err)
         })
     })       
+})
+
+router.post('/signin',(req,res)=>{
+    const {email,password} = req.body
+    if(!email || !password){
+        return res.status(422).json({error:"please add email or password"})
+    }
+    User.findOne({email:email})
+    .then(savedUser=>{
+        if(!savedUser){
+            return res.status(422).json({error:"please add email or password"})
+        }
+        bcrypt.compare(password,savedUser.password)
+        .then(doMatch=>{
+            if(doMatch){
+                res.json({message:"succesfully signed in"})
+            }
+            else{
+                return res.status(422).json({error:"please add email or password"})
+            }
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    })
 })
 
 module.exports = router
