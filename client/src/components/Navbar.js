ab536692c542bae4818966358cfaa781a06f28d8
Hashan Kumarasinghe <54454767+hashan99@users.noreply.github.com>
2020-12-10 19:17:46 +0530

    navbar links changed with after and before signin and signup

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import {Link} from 'react-router-dom'
+import {UserContext} from '../App'
 
 const NavBar = ()=>{
+    const {state,dispatch} = useContext(UserContext) //state is details of user
+    const renderList = ()=>{
+      if(state){
+        return [
+          <li><Link to="/profile">Profile</Link></li>,
+          <li><Link to="/create">Create Post</Link></li>
+        ]
+      }
+      else{
+        return [
+          <li><Link to="/signin">Signin</Link></li>,
+          <li><Link to="/signup">Signup</Link></li>
+        ]
+      }
+    }
+
     return(
       <div className="navbar-fixed">
         <nav>
         <div className="nav-wrapper white" style={{color:"Black"}}>
-          <Link to="/" className="brand-logo left">Instagram</Link>
+          <Link to={state?"/":"/signin"} className="brand-logo left">Instagram</Link>
           <ul id="nav-mobile" className="right">
-            <li><Link to="/signin">Signin</Link></li>
-            <li><Link to="/signup">Signup</Link></li>
-            <li><Link to="/profile">Profile</Link></li>
-            <li><Link to="/create">Create Post</Link></li>
+            {renderList()}
           </ul>
         </div>
         </nav>
