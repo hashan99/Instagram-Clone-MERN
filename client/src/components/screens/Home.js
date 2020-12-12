ea1d00eb50614786b55d934acdac9c1d161a6ded
Hashan Kumarasinghe <54454767+hashan99@users.noreply.github.com>
2020-12-12 11:59:18 +0530

    fetched posts on home page

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,22 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Home = () => {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        fetch("/allpost", {
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("jwt")
+            }
+        }).then(res => res.json())
+            .then(result => {
+                // console.log(result)
+                setData(result.posts)
+            })
+    }, [])
+
     return (
         <div className="home">
-            <div className="card home-card">
-                <h6>Hashan Chandima</h6>
-                <div className="card-amage">
-                    <img src="https://images.unsplash.com/photo-1491466424936-e304919aada7?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OHx8d2FsbHBhcGVyfGVufDB8MHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
-                </div>
-                <div className="card-content">
-                    <i className="material-icons" style={{ color: "red" }}>favorite</i>
-                    <h6>Nothern Lights</h6>
-                    <p>An aurora, sometimes referred to as polar lights, northern lights, or southern lights, is a natural light display in the Earth's sky, predominantly seen in high-latitude regions. Auroras are the result of disturbances in the magnetosphere caused by solar wind.</p>
-                    <input type="text" placeholder="add a comment" />
-                </div>
-            </div>
+            {
+                data.map(item => {
+                    return (
+                        <div className="card home-card" key={item._id}>
+                            <h6>{item.postedBy.name}</h6>
+                            <div className="card-image">
+                                <img src={item.photo} />
+                            </div>
+                            <div className="card-content">
+                                <i className="material-icons" style={{ color: "red" }}>favorite</i>
+                                <h6>{item.title}</h6>
+                                <p>{item.body}</p>
+                                <input type="text" placeholder="add a comment" />
+                            </div>
+                        </div>
+                    )
+                })
+            }
 
-            <div className="card home-card">
+
+            {/* <div className="card home-card">
                 <h6>Uththara Himansi</h6>
                 <div className="card-amage">
                     <img src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDF8fHdhbGxwYXBlcnxlbnwwfDB8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
@@ -40,7 +60,8 @@ const Home = () => {
                     <p>A glacier is a persistent body of dense ice that is constantly moving under its own weight. A glacier forms where the accumulation of snow exceeds its ablation over many years, often centuries.</p>
                     <input type="text" placeholder="add a comment" />
                 </div>
-            </div>
+            </div> */}
+
         </div>
     )
 }
