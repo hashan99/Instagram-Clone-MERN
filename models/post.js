b2d254086792ae117ebef9aac624c3ffe0fd12ef
Hashan Kumarasinghe <54454767+hashan99@users.noreply.github.com>
2020-12-15 01:35:45 +0530

    Node js API for commenting on post

--- models/post.js
+++ models/post.js
@@ -16,7 +16,10 @@ const postSchema = new mongoose.Schema({
     },
 
     likes:[{type:ObjectId,ref:"User"}],
-
+    comments:[{
+       text:String,
+       postedBy:{type:ObjectId,ref:"User"} 
+    }],
     postedBy:{
         type:ObjectId,
         ref:"User"
--- routes/post.js
+++ routes/post.js
@@ -61,6 +61,7 @@ router.put('/like',requireLogin,(req,res)=>{
         }
     })
 })
+
 router.put('/unlike',requireLogin,(req,res)=>{
     Post.findByIdAndUpdate(req.body.postId,{
         $pull:{likes:req.user._id}
@@ -75,5 +76,24 @@ router.put('/unlike',requireLogin,(req,res)=>{
     })
 })
 
+router.put('/comment',requireLogin,(req,res)=>{
+    const comment = {
+        text:req.body.text,
+        postedBy:req.user._id
+    }
+    Post.findByIdAndUpdate(req.body.postId,{
+        $push:{commentss:comment}
+    },{
+        new:true
+    })
+    .populate("comments:postedBy","_id name")
+    .exec((err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }else{
+            res.json(result)
+        }
+    })
+})
 
 module.exports = router
