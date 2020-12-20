0165fec8a3b4220d9fc8bd5755cad279bf6a21d0
Hashan Kumarasinghe <54454767+hashan99@users.noreply.github.com>
2020-12-21 04:58:27 +0530

    update profile img implemented

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -1,49 +1,98 @@
-import React,{useEffect,useState,useContext} from 'react'
-import {UserContext} from '../../App'
+import React, { useEffect, useState, useContext } from 'react'
+import { UserContext } from '../../App'
 
-const Profile = ()=>{
-    const [mypics,setPics] = useState([])
-    const {state,dispatch} = useContext(UserContext)
+const Profile = () => {
+    const [mypics, setPics] = useState([])
+    const { state, dispatch } = useContext(UserContext)
+    const [image, setImage] = useState("")
+    const [url, setUrl] = useState("")
     console.log(state)
-    useEffect(()=>{
-        fetch('/mypost',{
-            headers:{
-                "Authorization":"Bearer "+localStorage.getItem("jwt")
+    useEffect(() => {
+        fetch('/mypost', {
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("jwt")
             }
-        }).then(res=>res.json())
-        .then(result=>{
-            console.log(result)
-            setPics(result.mypost)
-        })
-    },[]) //empty dependancy array
-    return(
-        <div style={{maxWidth:"550px",margin:"0px auto"}}>
+        }).then(res => res.json())
+            .then(result => {
+                console.log(result)
+                setPics(result.mypost)
+            })
+    }, []) //empty dependancy array
+
+    useEffect(() => {
+        if (image) {
+            const data = new FormData()
+            data.append("file", image)
+            data.append("upload_preset", "instagram-clone")
+            data.append("cloud_name", "db957qd0z")
+            fetch("	https://api.cloudinary.com/v1_1/db957qd0z/image/upload", {
+                method: "post",
+                body: data
+            })
+                .then(res => res.json())
+                .then(data => {
+                    setUrl(data.url)
+                    console.log(data)
+                    localStorage.setItem("user",JSON.stringify({...state,pic:data.url}))
+                    dispatch({type:"UPDATEPIC",payload:data.url})
+                    window.location.reload()
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+    }, [image])
+
+    const updatePhoto = (file) => {
+        setImage(file)
+    }
+
+    return (
+        <div style={{ maxWidth: "550px", margin: "0px auto" }}>
             <div style={{
-                display:"flex",
-                justifyContent:"space-around",
-                margin:"18px 0px",
-                borderBottom:"1px solid grey"
+                margin: "18px 0px",
+                borderBottom: "1px solid grey"
             }}>
-                <div>
-                    <img style={{width:"160px",height:"160px",borderRadius:"80px"}} 
-                        src={state?state.pic:"loading"}
-                    />
+                <div style={{
+                    display: "flex",
+                    justifyContent: "space-around",
+                }}>
+                    <div>
+                        <img style={{ width: "160px", height: "160px", borderRadius: "80px" }}
+                            src={state ? state.pic : "loading"}
+                        />
+                    </div>
+                    <div>
+                        <h4>{state ? state.name : "loading"}</h4>
+                        <h6>{state ? state.email : "loading"}</h6>
+                        <div style={{ display: "flex", justifyContent: "space-between", width: "108%" }}>
+                            <h6>{mypics.length} Posts</h6>
+                            <h6>{state ? state.followers.length : "0"} Followers</h6>
+                            <h6>{state ? state.following.length : "0"} Following</h6>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <h4>{state?state.name:"loading"}</h4>
-                    <h6>{state?state.email:"loading"}</h6>
-                    <div style={{display:"flex",justifyContent:"space-between",width:"108%"}}>
-                        <h6>{mypics.length} Posts</h6>
-                        <h6>{state?state.followers.length:"0"} Followers</h6>
-                        <h6>{state?state.following.length:"0"} Following</h6>
+                {/* <button className="btn waves-effect waves-light #64b5f6 blue darken-1" 
+                style={{margin: "10px 0px 10px 48px"}}
+                onClick={()=>{
+                    updatePhoto()
+                }}
+                >Update Profile
+            </button> */}
+                <div className="file-field input-field" style={{ margin: "10px" }}>
+                    <div className="btn #64b5f6 blue darken-1">
+                        <span>Update Profile</span>
+                        <input type="file" onChange={(e) => updatePhoto(e.target.files[0])} />
+                    </div>
+                    <div className="file-path-wrapper">
+                        <input className="file-path validate" type="text" />
                     </div>
                 </div>
             </div>
-        
             <div className="gallery">
                 {
-                    mypics.map(item=>{
-                        return(
+                    mypics.map(item => {
+                        return (
                             <img key={item._id} className="item" src={item.photo} alt={item.title} />
                         )
                     })
