224de0b14fadbf46927950d7669c5c83a527d083
Hashan Kumarasinghe <54454767+hashan99@users.noreply.github.com>
2020-12-24 04:27:17 +0530

    show latest post on top implemented

--- routes/post.js
+++ routes/post.js
@@ -8,6 +8,7 @@ router.get('/allpost',requireLogin,(req,res)=>{
     Post.find()
     .populate("postedBy","_id name")
     .populate("comments.postedBy","_id name")
+    .sort('-createdAt') //minus symbol for descending order
     .then(posts=>{
         res.json({posts})
     })
@@ -22,6 +23,7 @@ router.get('/followingpost',requireLogin,(req,res)=>{
     Post.find({postedBy:{$in:req.user.following}})
     .populate("postedBy","_id name")
     .populate("comments.postedBy","_id name")
+    .sort('-createdAt') //minus symbol for descending order
     .then(posts=>{
         res.json({posts})
     })
