e99b27f023d8e99185fa3eb1ec68f3e2fda51d38
Hashan Kumarasinghe <54454767+hashan99@users.noreply.github.com>
2021-01-03 22:35:40 +0530

    try to add particles

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -110,6 +110,9 @@ const Signup = () => {
                 <h5>
                     <Link to="/signin">Already have an account ?</Link>
                 </h5>
+                <h6>
+                    <Link to="/reset">Forgot Password ?</Link>
+                </h6>
             </div>
         </div>
     )
