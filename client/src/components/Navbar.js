805b515c08eeba9fb5849709c3f61ff717595cb5
Hashan Kumarasinghe <54454767+hashan99@users.noreply.github.com>
2021-01-04 11:47:29 +0530

    search user client side implemented

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,21 @@
-import React,{useContext} from 'react'
+import React,{useContext,useRef,useEffect,useState} from 'react'
 import {Link,useHistory} from 'react-router-dom'
 import {UserContext} from '../App'
+import M from 'materialize-css'
 
 const NavBar = ()=>{
+    const searchModal = useRef(null)
+    const [search,setSearch] = useState('')
     const {state,dispatch} = useContext(UserContext) //state is details of user
     const history = useHistory()
+    useEffect(()=>{
+      M.Modal.init(searchModal.current)
+    },[])
     const renderList = ()=>{
       if(state){
         return [
-          <li><Link to="/profile">
+          <li key="1"><i data-target="modal1" className="large material-icons modal-trigger" style={{color:"black"}}>search</i></li>,
+          <li key="2"><Link to="/profile">
             <img class="nav-icon"
                 src="https://res.cloudinary.com/db957qd0z/image/upload/v1608644600/logos/Profile_vd3snf.ico" 
                 weight="30px" height="30px"
@@ -16,7 +23,7 @@ const NavBar = ()=>{
             {/* Profile */}
           </Link></li>,
 
-          <li><Link to="/create">
+          <li key="3"><Link to="/create">
             <img class="nav-icon"
                 src="https://res.cloudinary.com/db957qd0z/image/upload/v1608645252/logos/Create_xm963o.png" 
                 weight="30px" height="30px"
@@ -24,14 +31,14 @@ const NavBar = ()=>{
             {/* Create Post */}
           </Link></li>,
 
-          <li><Link to="/myfollowingpost">
+          <li key="4"><Link to="/myfollowingpost">
             <img class="nav-icon"
                 src="https://res.cloudinary.com/db957qd0z/image/upload/v1608645772/logos/MyFollowing_iqz6sz.ico" 
                 weight="30px" height="30px"
             ></img>
             {/* My Following Posts */}
           </Link></li>,
-          <li>
+          <li key="5">
             {/* <button className="btn #c62828 red darken-3" 
                 onClick={()=>{
                 localStorage.clear()
@@ -56,8 +63,8 @@ const NavBar = ()=>{
       }
       else{
         return [
-          <li><Link to="/signin">Signin</Link></li>,
-          <li><Link to="/signup">Signup</Link></li>
+          <li key="6"><Link to="/signin">Signin</Link></li>,
+          <li key="7"><Link to="/signup">Signup</Link></li>
         ]
       }
     }
@@ -75,6 +82,19 @@ const NavBar = ()=>{
           <ul id="nav-mobile" className="right">
             {renderList()}
           </ul>
+        </div>
+        <div id="modal1" class="modal" ref={searchModal} style={{color:"black"}}>
+          <div className="modal-content">
+            <input 
+              type="text" 
+              placeholder="search user" 
+              value={search}
+              onChange={(e)=>setSearch(e.target.value)}
+            />
+          </div>
+          <div className="modal-footer">
+            <button  className="modal-close waves-effect waves-green btn-flat">Agree</button>
+          </div>
         </div>
         </nav>
       </div>
