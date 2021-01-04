f7f383306578a3dfc28fabf8cab5d91dbb3314ef
Hashan Kumarasinghe <54454767+hashan99@users.noreply.github.com>
2021-01-04 20:12:35 +0530

    search-users route implemented

--- routes/user.js
+++ routes/user.js
@@ -74,4 +74,15 @@ router.put('/updatepic',requireLogin,(req,res)=>{
         })
 })
 
+router.post('/search-users',(req,res)=>{
+    let userPatern = new RegExp("^"+req.body.query)
+    User.find({email:{$regex:userPatern}})
+    .select("_id email")
+    .then(user=>{
+        res.json({user})
+    }).catch(err=>{
+        console.log(err)
+    })
+})
+
 module.exports = router
